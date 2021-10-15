5e580f00773ab644d87923cc148f912bf0037ca4
Alberto-Funk <laptopreneur@gmail.com>
2021-10-15 00:51:49 -0700

    styles+structure - setting up nav and filter buttons with their menus - had to extend min height/width since the only option was 0px - I want it to have the same utility options as regular height - but I cannot find info on how to extend a utility by - referencing another/using its options - adding them statically for now

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -17,48 +17,64 @@ function MainHeader() {
   return (
     <div className="h-16 flex-shrink-0 bg-white">
       <header className="h-16 fixed top-0 right-0 left-0 bg-transparent text-center">
-        <div className="absolute left-0 top-0 -bottom-1.5 bg-red-100 rounded-br-full">
-          <button type="button" aria-expanded={!isNavOpen} onClick={toggleNav}>
-            open navigation
+        <div
+          className={`min-h-20 min-w-20 fixed bg-red-100 rounded-br-full ${
+            isNavOpen ? "inset-0" : "left-0 top-0"
+          }`}
+        >
+          <button
+            type="button"
+            className="h-14 w-14 absolute left-0 top-0 flex justify-center items-center bg-blue-100"
+            aria-expanded={!isNavOpen}
+            onClick={toggleNav}
+          >
+            <span className="sr-only">open navigation</span>
+            <span className="flex h-11 w-11 bg-yellow-100"></span>
           </button>
-        </div>
 
-        <nav className={`${isNavOpen ? "" : "hidden"}`}>
-          <ul>
-            <li>
-              <Link href="/">
-                <a>home</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/food">
-                <a>food</a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/activities">
-                <a>activities</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+          <nav className={`${isNavOpen ? "" : "hidden"}`}>
+            <ul>
+              <li>
+                <Link href="/">
+                  <a>home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/food">
+                  <a>food</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/activities">
+                  <a>activities</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
         <Link href="/">
           <a>NomCue</a>
         </Link>
 
-        <div className="absolute right-0 top-0 -bottom-1.5 bg-red-100 rounded-bl-full">
+        <div
+          className={`min-h-20 min-w-20 fixed bg-red-100 rounded-bl-full ${
+            areFiltersOpen ? "inset-0" : "right-0 top-0"
+          }`}
+        >
           <button
             type="button"
+            className="h-14 w-14 absolute right-0 top-0 flex justify-center items-center bg-blue-100"
             aria-expanded={!areFiltersOpen}
             onClick={toggleFilters}
           >
-            open filters
+            <span className="sr-only">open filters</span>
+            <span className="flex h-11 w-11 bg-yellow-100"></span>
           </button>
-        </div>
 
-        <div className={`${areFiltersOpen ? "" : "hidden"}`}>
-          <LocationAutocomplete />
+          <div className={`${areFiltersOpen ? "" : "hidden"}`}>
+            <LocationAutocomplete />
+          </div>
         </div>
       </header>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      minHeight: {
+        '20': '5rem',
+      },
+      minWidth: {
+        '20': '5rem',
+      },
+    },
   },
   variants: {
     extend: {},
